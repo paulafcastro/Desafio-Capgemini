c613d57ffd5e8d7938fcdb5f309a9ba95d0fcd5d
Paula Castro <83676566+paulafcastro@users.noreply.github.com>
2021-07-04 19:55:03 -0300

    Add files via upload

---
+++ calculadora.php
@@ -0,0 +1,14 @@
+<?php
+      $valorInvestido = 10;
+      $visualizacoes = round(100 / 12);
+      $cliquesParaCompartilhamento = round(20 / 3);
+
+      $anuncioPrincipal = $valorInvestido * 30;
+      $cliques = round($anuncioPrincipal / $visualizacoes);
+      $compartilhamento = round($cliques / $cliquesParaCompartilhamento);
+      $novasVisualizacoes = $compartilhamento * 40;
+
+      $totalDeVisualizacoes = $anuncioPrincipal + $novasVisualizacoes;
+
+    echo "Aproximadamente" . " " . $totalDeVisualizacoes . " " . "pessoas verão o seu anúncio!";
+
